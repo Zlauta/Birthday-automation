848923002f8adcfb32a59eb992915b5f6c72e193
Zlauta <Zlauuta23@gmail.com>
2025-12-24 12:39:44 -0300

    style: apply code formatting with Prettier throughout the project

--- app/api/birthdays-banner/route.js
+++ app/api/birthdays-banner/route.js
@@ -1,21 +1,26 @@
-import { birthdayManager } from '@/service/birthdayManager.service.js';
-import { NextResponse } from 'next/server';
+import { birthdayManager } from "@/service/birthdayManager.service.js";
+import { NextResponse } from "next/server";
 
 export async function POST() {
-    try {
-        const processed = await birthdayManager.processDailyBirthdays();
+  try {
+    const processed = await birthdayManager.processDailyBirthdays();
 
-        if (processed.length === 0) {
-            return NextResponse.json({ message: "No hay cumpleañeros hoy." }, { status: 200 });
-        }
-
-        return NextResponse.json({ 
-            status: "success", 
-            data: processed 
-        }, { status: 201 });
-
-    } catch (error) {
-        console.error("Error:", error);
-        return NextResponse.json({ error: "Error interno" }, { status: 500 });
+    if (processed.length === 0) {
+      return NextResponse.json(
+        { message: "No hay cumpleañeros hoy." },
+        { status: 200 }
+      );
     }
+
+    return NextResponse.json(
+      {
+        status: "success",
+        data: processed,
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error("Error:", error);
+    return NextResponse.json({ error: "Error interno" }, { status: 500 });
+  }
 }
