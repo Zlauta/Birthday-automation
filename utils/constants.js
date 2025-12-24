bee136e2998aea95e58445f45cac5b53748864bd
Zlauta <Zlauuta23@gmail.com>
2025-12-24 12:18:05 -0300

    Refactor: Move the month names constant to constants.js to centralize the configuration

--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,10 @@
 export const BANNER_APP_URL = 'https://demos.rollingcodeschool.com/welcome-app/';
 export const DUMMY_IMAGE_URL = 'https://i.pravatar.cc/300';
 export const DOWNLOADS_DIR_NAME = 'downloads';
+export const MONTH_NAMES = [
+  "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+  "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+];
 
 export const COLUMN_NAMES = {
     FULL_NAME: 'Apellido y Nombre',
